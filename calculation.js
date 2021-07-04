9c4256e53d4d9c48c284d72901bc987b557ae8e2
janeishahu <janeishahu@aol.com>
2021-07-04 15:24:15 -0400

    ran test and failed

--- calculation.js
+++ calculation.js
@@ -1,1 +1,13 @@
+function total(num1, num2) {
+  let num1Type = typeof num1
+  let num2Type = typeof num2
 
+  // eslint-disable-next-line eqeqeq
+  if (num1 == num2 && num1Type == num2Type) {
+    return true
+  }
+
+  return false
+}
+
+module.exports = total

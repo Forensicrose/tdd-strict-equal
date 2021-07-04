23457da148e998facd5d1bc406ad95914a1de16c
janeishahu <janeishahu@aol.com>
2021-07-04 15:22:38 -0400

    set up calculation.test. js, added tests folder and calculation.js

---
+++ tests/calculation.test.js
@@ -0,0 +1,16 @@
+let describe = require('mocha').describe
+let it = require('mocha').it
+let expect = require('chai').expect
+var strictEquals = require('../calculation')
+
+describe('calculation', () => {
+  describe('strictEqual', () => {
+    it('returns true if the values are strictly equal to each other', () => {
+      let num1 = 5
+      let num2 = 5
+      let total = strictEquals(num1, num2)
+
+      expect(total(num1, num2)).to.equal(true)
+    })
+  })
+})
